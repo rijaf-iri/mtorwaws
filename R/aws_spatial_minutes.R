a983d6a7e2fa127a81aaf75d4aa3f2dd585b546a
rijaf-iri <rijaf@iri.columbia.edu>
2020-11-29 14:53:59 -0500

    change spatial files creation

--- R/aws_spatial_minutes.R
+++ R/aws_spatial_minutes.R
@@ -18,8 +18,8 @@ aws_spatial_minutes <- function(dirAWS){
     timeNow <- Sys.time()
     daty2 <- paste0(substr(format(timeNow, "%Y%m%d%H%M"), 1, 11), 0)
     daty2 <- strptime(daty2, "%Y%m%d%H%M", tz = tz)
-    ## operational last 12 hours
-    timeLast <- timeNow - 43200
+    ## operational last 6 hours
+    timeLast <- timeNow - 21600
     daty1 <- paste0(substr(format(timeLast, "%Y%m%d%H%M"), 1, 11), 0)
     daty1 <- strptime(daty1, "%Y%m%d%H%M", tz = tz)
 
